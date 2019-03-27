c3ad6f29b78631b2532820bea008e3f8412284d9
HiAugust <47995491+HiAugust@users.noreply.github.com>
2019-03-26 20:54:57 -0400

    Add files via upload

---
+++ js/site.js
@@ -0,0 +1,20 @@
+console.log("External file");
+
+var city;
+var cityFrom = "";
+var cityName = "New York City";
+var x = 32;
+
+console.log("cityName, x: ", cityName, x);
+
+var firstname = "Hector", lastname = "Agosto";
+console.log("firstname, lastname", firstname, lastname);
+
+var s = "2019";
+var s2 = 2019;
+var n = 2001;
+console.log ("age: ", s - n);
+console.log ("age: ", s + n);
+console.log("age:  ", s2 + n);
+var escapequotes = "This is Judi's \"city\" name";
+console.log ("escapequotes: ", escapequotes);
